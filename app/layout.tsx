07b08b4c21236c623bd993c82acb9e9ee4b49f5a
Retaehc-pop <papop2003@gmail.com>
2023-10-16 00:45:59 +0200

    main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import './globals.scss'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import AuthProvider from '@/components/AuthProvider'
-
+import Link from 'next/link'
+import SignInButton from '@/components/SignInButton'
+import styles from './layout.module.scss'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,11 +20,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>
           <AuthProvider>
+            <nav className={styles.nav}>
+              <ul>
+                <li>
+                  <Link href="/" passHref>Home</Link>
+                </li>
+                <li>
+                  <SignInButton />
+                </li>
+              </ul>
+            </nav>
             {children}
           </AuthProvider>
-        </main>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,28 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.scss'
 import Link from 'next/link'
+import SignInButton from '@/components/SignInButton'
+import { useSession } from 'next-auth/react'
+export default function Home() {
 
-
-
-export default async function Home() {
+  const { data: session } = useSession()
+  
 
   return (
     <main className={styles.main}>
-      
+    {
+      session ? (
+        <div>
+          login
+        </div>
+        ):(
+          <div>
+            not login
+            </div>
+        )
+    }
     </main>
+
   )
 }
